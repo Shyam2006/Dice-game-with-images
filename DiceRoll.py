30604e571c70e519b8802dc8f8f20fa3677d11ad
Shyam2006 <73068800+Shyam2006@users.noreply.github.com>
2021-05-26 00:51:42 +0530

    Delete DiceRoll.py

--- DiceRoll.py
+++
@@ -1,8 +0,0 @@
-#Dice Game with Images
-import random
-from PIL import Image
-dice=random.randint(1,6)
-picname = "images\\" + str(dice) + ".png"
-print("dice = ", dice)
-img = Image.open(picname)
-img.show()
